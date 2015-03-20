c0338a5e95c90683220a1867b90e90fc98cb2af0
Bartesto <barthuntley@iinet.net.au>
2015-03-20 12:50:26 +0800

    Re added mod 6_3 and not running orig now as processing in ARCGIS changed

--- Roost_validation_analysis_crown_pts.R
+++ Roost_validation_analysis_crown_pts.R
@@ -3,13 +3,40 @@ rm(list=ls())
 
 library(dplyr)
 library(ggplot2)
+library(xtable)
 
 
 df <- read.table("UM_valid_cc_roost_NEAR_crown_pts.txt", sep = ",", header = TRUE)
+df_no <- read.table("UM_valid_cc_roost_NEAR_crown_pts_NO.txt", sep = ",", header = TRUE)
+df_5_3 <- read.table("UM_valid_cc_roost_NEAR_crown_pts_5_3.txt", sep = ",", header = TRUE)
+df_6_3 <- read.table("UM_valid_cc_roost_NEAR_crown_pts_6_3.txt", sep = ",", header = TRUE)
 
-crown_score <- df %>%
+
+orig <- df %>%
                 select(UM_FID_ID, dist_to_cr)%>%
                 group_by(UM_FID_ID)%>%
-                summarise(crown_score = sum(dist_to_cr))
+                summarise(cs = sum(dist_to_cr))
 
-crown_score
+mod_no <- df_no %>%
+        select(UM_FID_ID, dist_to_cr)%>%
+        group_by(UM_FID_ID)%>%
+        summarise(cs_no = sum(dist_to_cr))
+
+mod_5_3 <- df_5_3 %>%
+        select(UM_FID_ID, dist_to_cr)%>%
+        group_by(UM_FID_ID)%>%
+        summarise(cs_5_3 = sum(dist_to_cr))
+
+mod_6_3 <- df_6_3 %>%
+        select(UM_FID_ID, dist_to_cr)%>%
+        group_by(UM_FID_ID)%>%
+        summarise(cs_6_3 = sum(dist_to_cr))
+
+csALL <- cbind(orig, mod_no[,2], mod_5_3[,2], mod_6_3[,2])
+
+mods <- colSums(csALL[,2:length(csALL)])
+
+
+table <- xtable(csALL)
+
+print.xtable(table, type="html", file="test.html")
